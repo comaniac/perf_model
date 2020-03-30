15ee6efe5d3f0e440f7965f3ea405ec1063eb898
Xingjian Shi <xshiab@connect.ust.hk>
2020-03-30 00:21:05 -0700

    Update learning_to_rank_param_search.sh

--- tests/learning_to_rank_param_search.sh
+++ tests/learning_to_rank_param_search.sh
@@ -1,18 +1,23 @@
 NUM_GPU=8
 i=0
 
-for num_hidden in 32 64 128 256 512
+for num_hidden in 64 128 256
 do
   for num_layers in 1 2 3
   do
-    for threshold in 1 5
+    for dropout in 0.1 0.2
     do
-      screen -d -m python ../thrpt_model.py --dataset ../depthwise_conv2d_nchw.cuda-cuda-model-t4.csv \
-        --algo nn \
-        --gpus ${i} \
-        --threshold ${threshold} --num_hidden ${num_hidden} --num_layers ${num_layers} \
-        --out_dir thrpt_h${num_hidden}_l${num_layers}_tr${threshold}
-      ((i=(i+1)%NUM_GPU))
+      for threshold in 1 5
+      do
+        screen -d -m python ../thrpt_model.py --dataset ../depthwise_conv2d_nchw.cuda-cuda-model-t4.csv \
+          --algo nn \
+          --gpus ${i} \
+          --threshold ${threshold} --num_hidden ${num_hidden} \
+          --dropout ${dropout} \
+          --num_layers ${num_layers} \
+          --out_dir thrpt_h${num_hidden}_d${dropout}_l${num_layers}_tr${threshold}
+        ((i=(i+1)%NUM_GPU))
+      done
     done
   done
 done
