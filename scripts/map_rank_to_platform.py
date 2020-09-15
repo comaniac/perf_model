c68703ecdb876846d595a118c4fa449ad05c4152
Cody Yu <comanaic0422@gmail.com>
2020-09-15 18:21:35 +0000

    Ranking evalution script

--- scripts/map_rank_to_platform.py
+++ scripts/map_rank_to_platform.py
@@ -18,23 +18,30 @@ top_n_cfgs = 32
 platform1_dir = sys.argv[1]
 
 # The config folder of the second platform.
-platform2_dir = sys.argv[2]
+# When the target config is missing in the full log,
+# we may re-tune it and put it to another log file.
+# In short, when two platform2 dirs are specified,
+# we will aggregate their logs as the reference.
+platform2_dirs = [sys.argv[2]]
+if len(sys.argv) == 4:
+    platform2_dirs.append(sys.argv[3])
 
 log_target = None
-for log_file in glob.glob('{}/*'.format(platform2_dir)):
+for log_file in glob.glob('{}/*'.format(platform2_dirs[0])):
     for inp, res in load_from_file(log_file):
         log_target = str(inp.target)
         break
     break
+print(log_target)
 
 # Histogram of rank shifting counts
 hist = {}
 
-def run_one_wkl(best_config_log, all_log_dir):
+def run_one_wkl(platform1_log, platform2_dirs):
     target_wkl = None
     cfgs = []
     log_files = None
-    for inp, res in load_from_file(best_config_log):
+    for inp, res in load_from_file(platform1_log):
         # Update the target string to generate the same SHA2 hash code
         # to identify the full config log file.
         if target_wkl is None:
@@ -44,11 +51,13 @@ def run_one_wkl(best_config_log, all_log_dir):
     
             # The full config log file name by Lorien is composed of
             # <SHA2>-<5-byte UUID4>.json
-            log_path = '{0}/{1}*.json'.format(all_log_dir, target_wkl.hash_sha2())
-            log_files = glob.glob(log_path)
+            log_files = []
+            for platform2_dir in platform2_dirs:
+                log_path = '{0}/{1}*.json'.format(platform2_dir, target_wkl.hash_sha2())
+                log_files += glob.glob(log_path)
             if not log_files:
-                print('Log missing for %s' % str(target_wkl))
-                return -10
+                print('Log missing for %s: %s' % (str(target_wkl), target_wkl.hash_sha2()))
+                return
     
         # Only focus on the best N configs.
         target_cfg_str = str(inp.config)
@@ -63,6 +72,7 @@ def run_one_wkl(best_config_log, all_log_dir):
     all_records = {}
     for log_file in log_files:
         for inp, res in load_from_file(log_file):
+            # De-duplication.
             cfg = str(inp.config)
             if cfg not in all_records:
                 all_records[cfg] = (inp, res)
@@ -74,14 +84,14 @@ def run_one_wkl(best_config_log, all_log_dir):
     mapped = [False for _ in range(len(cfg_to_rank_on_p2) + 1)]
     
     assert target_wkl is not None
-    task = target_wkl.to_task()
-    space_size = np.product([len(v.entities) for v in task.config_space.space_map.values()])
+    #task = target_wkl.to_task()
+    #space_size = np.product([len(v.entities) for v in task.config_space.space_map.values()])
     
     cfgs.sort(key=lambda x: -x[0])
     for rank1, (target_cost, target_cfg_str) in enumerate(cfgs):
         target_cost = -target_cost
     
-        display_name = '{} {} rank1 {}'.format(str(task), target_cfg_str, rank1)
+        #display_name = '{} {} rank1 {}'.format(str(task), target_cfg_str, rank1)
     
         # Map the rank from the first platform to the second platform.
         if target_cfg_str not in cfg_to_rank_on_p2:
@@ -96,16 +106,13 @@ def run_one_wkl(best_config_log, all_log_dir):
             hist[rank_shift] = 0
         hist[rank_shift] += 1
 
-        if rank_shift <= -2000:
-            print('{}\trank2 {:5d}\t{:5d}\t{:10d}'.format(display_name, rank2, len(all_records), space_size))
-    
-    #if not all(mapped):
-    #    print('%d configs on patform 2 are not mapped' % sum([m is False for m in mapped]))
+        #if rank_shift <= -2000:
+        #    print('{}\trank2 {:5d}\t{:5d}\t{:10d}'.format(display_name, rank2, len(all_records), space_size))
 
     return
 
 for config_file in glob.glob('{}/*.json'.format(platform1_dir)):
-    run_one_wkl(config_file, platform2_dir)
+    run_one_wkl(config_file, platform2_dirs)
 
 with open('out.log', 'w') as filep:
     for rank_shift, cnt in sorted(hist.items(), key=lambda item: item[0]):
---
+++ scripts/rank_tester.py
@@ -0,0 +1,80 @@
+import glob
+import heapq
+import math
+import os
+import sys
+import tqdm
+
+import numpy as np
+
+import tvm
+from tvm import autotvm
+from tvm.autotvm.record import load_from_file
+from tvm.autotvm.measure import create_measure_batch, MeasureInput
+from tvm.autotvm.tuner.callback import log_to_file
+from lorien.workload import Workload, gen_log_file_name_from_workload
+
+# Number of top configs.
+top_n_cfgs = 32
+
+# The config of the platform.
+platform1_dir = sys.argv[1]
+
+platform2_dir = sys.argv[2]
+
+log_target = sys.argv[3]
+
+wkls = {}
+
+def parse_one_log(best_config_log, new_log_dir):
+    target_wkl = None
+    for inp, res in load_from_file(best_config_log):
+        # Update the target string to generate the right SHA2 hash code.
+        if target_wkl is None:
+            inp.task.target = inp.target
+            target_wkl = Workload.from_task(inp.task)
+            target_wkl['target'] = log_target
+            if target_wkl not in wkls:
+                new_log_file_name = gen_log_file_name_from_workload(target_wkl)
+                new_log_path = '{0}/{1}'.format(new_log_dir, new_log_file_name)
+                wkls[target_wkl] = (new_log_path, [])
+
+        if res.error_no != 0:
+            continue
+    
+        # Only focus on the best N configs.
+        new_inp = MeasureInput(target=log_target, task=inp.task, config=inp.config)
+        if len(wkls[target_wkl][1]) < top_n_cfgs:
+            heapq.heappush(wkls[target_wkl][1], (-np.mean(res.costs), new_inp))
+        elif np.mean(res.costs) < -wkls[target_wkl][1][0][0]:
+            heapq.heappop(wkls[target_wkl][1])
+            heapq.heappush(wkls[target_wkl][1], (-np.mean(res.costs), new_inp))
+
+
+def run_one_wkl(wkl, new_log_path, inputs):
+    task = wkl.to_task()
+
+    # Re-tune the best configs.
+    log_writter = log_to_file(new_log_path)
+    measure_option = autotvm.measure_option(
+        builder=autotvm.LocalBuilder(timeout=10),
+        runner=autotvm.LocalRunner(number=5, repeat=1, min_repeat_ms=1000)
+    )
+    measure_batch = create_measure_batch(task, measure_option)
+    results = measure_batch(inputs)
+    log_writter(None, inputs, results)
+
+    del measure_batch
+    return
+
+if not os.path.exists(platform2_dir):
+    os.mkdir(platform2_dir)
+
+for config_file in tqdm.tqdm(glob.glob('{}/*.json'.format(platform1_dir))):
+    parse_one_log(config_file, platform2_dir)
+
+for wkl, (new_log_path, heap) in tqdm.tqdm(wkls.items()):
+    if not heap:
+        continue
+    run_one_wkl(wkl, new_log_path, [item[1] for item in heap])
+
